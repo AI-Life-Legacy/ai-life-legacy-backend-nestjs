61af5947ea1a18d0a204c1cdb2fc0e2489b63b5a
blueTino <geunjun04@gmail.com>
2025-11-14 15:57:06 +0900

    chore: superbase 연동 추가

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -9,14 +9,31 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule], // ConfigModule을 가져옴
       inject: [ConfigService], // ConfigService를 주입받음
       useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
+
+        /* mySQL 설정 예시 
+        type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         entities: [__dirname + '/entity/*.entity{.ts,.js}'],
+        synchronize: false, */
+
+        // superbase 설정
+        type: 'postgres',
+
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT') ?? 5432),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/entity/*.entity{.ts,.js}'],
         synchronize: false,
+        // Supabase는 SSL 필요
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
     }),
   ],
